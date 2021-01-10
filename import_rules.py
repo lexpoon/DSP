194c50e1cd35c552cf594eb788ad24f37c6f93d3
Mark <mark.9696@hotmail.com>
2021-01-10 16:21:26 +0100

    Updated vector decision. Now easy to add new vectors. Weights have also been adjusted to force bigger increases.

--- import_rules.py
+++ import_rules.py
@@ -34,107 +34,42 @@ def clean_column(df, column):
 ''' STANDAARD '''
 from ast import literal_eval
 
-def create_true_falses_addition(row):
+def create_true_falses_multiplication(row):
     data = literal_eval(row['facilities'])
+
+    # These are the vector updates based on the facilities column
     if 'MuseumLibrary' in data:
-        row['bieb'] = 3
+        row['bieb'] = 1
     else:
         row['bieb'] = 0
     if 'OpenAir' in data:
-        row['openair'] = 3
+        row['openair'] = 1
     else:
         row['openair'] = 0
     if 'Parking' in data:
-        row['parking'] = 7
+        row['parking'] = 1
     else:
         row['parking'] = 0
     if 'WheelchairAccess' in data:
-        row['weelchair'] = 10
+        row['weelchair'] = 1
     else:
         row['weelchair'] = 0
     if 'AccessibleDisabled' in data:
-        row['disabled'] = 5
+        row['disabled'] = 1
     else:
         row['disabled'] = 0
     if 'NearTrainStation' in data:
-        row['trainstation'] = 8
+        row['trainstation'] = 1
     else:
         row['trainstation'] = 0
     if 'Restaurant' in data:
         row['restaurant'] = 1
     else:
         row['restaurant'] = 0
-    return row
-
-
-def create_true_falses_multiplication(row):
-    data = literal_eval(row['facilities'])
-
-    # These are the vector updates based on the facilities column
-    if 'MuseumLibrary' in data:
-        row['bieb'] = 2
-    else:
-        row['bieb'] = 1
-    if 'OpenAir' in data:
-        row['openair'] = 4
-    else:
-        row['openair'] = 1
-    if 'Parking' in data:
-        row['parking'] = 2
-    else:
-        row['parking'] = 1
-    if 'WheelchairAccess' in data:
-        row['weelchair'] = 4
-    else:
-        row['weelchair'] = 1
-    if 'AccessibleDisabled' in data:
-        row['disabled'] = 5
-    else:
-        row['disabled'] = 1
-    if 'NearTrainStation' in data:
-        row['trainstation'] = 3
-    else:
-        row['trainstation'] = 1
-    if 'Restaurant' in data:
-        row['restaurant'] = 2
-    else:
-        row['restaurant'] = 1
 
     # These are the vector updates based on the categories found in the external museum data file
 
     return row
-
-# def create_true_falses_multiplication(row):
-#     data = literal_eval(row['facilities'])
-#     if 'MuseumLibrary' in data:
-#         row['bieb'] = 2.07
-#     else:
-#         row['bieb'] = 1
-#     if 'OpenAir' in data:
-#         row['openair'] = 2.53
-#     else:
-#         row['openair'] = 1
-#     if 'Parking' in data:
-#         row['parking'] = 2.1
-#     else:
-#         row['parking'] = 1
-#     if 'WheelchairAccess' in data:
-#         row['weelchair'] = 3.26
-#     else:
-#         row['weelchair'] = 1
-#     if 'AccessibleDisabled' in data:
-#         row['disabled'] = 2.93
-#     else:
-#         row['disabled'] = 1
-#     if 'NearTrainStation' in data:
-#         row['trainstation'] = 3.26
-#     else:
-#         row['trainstation'] = 1
-#     if 'Restaurant' in data:
-#         row['restaurant'] = 1.88
-#     else:
-#         row['restaurant'] = 1
-#     return row
 
 def create_dataframe(filename):
 
@@ -146,27 +81,16 @@ def create_dataframe(filename):
 def apply_onehot(df):
     one_hot_cat = df.museaal_thema.str.get_dummies(', ')
     df = pd.concat([df, one_hot_cat], axis=1)
+    print(df.info())
     return df
 
-def create_lists(df, x):
+def create_lists(df, x, addition):
     mylist = df[f'{x}'].tolist()
     myarray = np.array(mylist)
+    myarray *= addition
+    myarray += 1
     return myarray
 
-def create_row_vectors(row):
-
-    index = row['index']
-    vector = np.zeros(496, dtype=object)
-    vector += 1
-    vector *= bieb_array
-    vector *= openair_array
-    vector *= parking_array
-    vector *= weelchair_array
-    vector *= disabled_array
-    vector *= trainstation_array
-    vector *= restaurant_array
-    vector[index] = 1
-    return vector
 
 def create_row_vectors(row):
 
@@ -187,8 +111,24 @@ def create_row_vectors(row):
         vector *= trainstation_array
     if row['restaurant'] != 0:
         vector *= restaurant_array
+    if row['History'] != 0:
+        vector *= restaurant_array
+    if row['Visual'] != 0:
+        vector *= restaurant_array
+    if row['Culture'] != 0:
+        vector *= restaurant_array
+    if row['Naval'] != 0:
+        vector *= restaurant_array
+    if row['Nature'] != 0:
+        vector *= restaurant_array
+    if row['Tech'] != 0:
+        vector *= restaurant_array
+    if row['Ethnology'] != 0:
+        vector *= restaurant_array
+
     vector[index] = 1
     return vector
+
 # Create the dataframe from the museum file and so some cleaning
 filename = 'musea.csv'
 df = create_dataframe(filename)
@@ -196,28 +136,33 @@ df = df.drop_duplicates(subset=['publicName'])
 df.rename(columns={'Unnamed: 0': "index"}, inplace=True)
 # Import the extra categories from the visitor file
 df_visitor = create_dataframe('Notebooks/museua_visitors.csv')
-df = df.merge(df_visitor, on='translationSetId')
-
+df = df.merge(df_visitor, how='left', left_on='translationSetId', right_on='translationSetId')
+df = df.fillna(0)
 # Add all the facilites as on its own columns
 df[['bieb', 'openair', 'parking', 'weelchair', 'disabled', 'trainstation', 'restaurant']] = False
 # For every facility add the increase/weight/update value which will be converted to the update arrays
 df = df.apply(create_true_falses_multiplication, axis=1)
 
 # Create the update arrays for all the facilities
-bieb_array = create_lists(df, 'bieb')
-openair_array = create_lists(df, 'openair')
-parking_array = create_lists(df, 'parking')
-weelchair_array = create_lists(df, 'weelchair')
-disabled_array = create_lists(df, 'disabled')
-trainstation_array = create_lists(df, 'trainstation')
-restaurant_array = create_lists(df, 'restaurant')
-
-
+bieb_array = create_lists(df, 'bieb', 2)
+openair_array = create_lists(df, 'openair', 10)
+parking_array = create_lists(df, 'parking', 8)
+weelchair_array = create_lists(df, 'weelchair', 12)
+disabled_array = create_lists(df, 'disabled', 15)
+trainstation_array = create_lists(df, 'trainstation', 7)
+restaurant_array = create_lists(df, 'restaurant', 1)
+history = create_lists(df, 'History', 2)
+visual = create_lists(df, 'Visual', 3)
+culture = create_lists(df, 'Culture', 3)
+naval = create_lists(df, 'Naval', 4)
+nature = create_lists(df, 'Nature', 4)
+tech = create_lists(df, 'Tech', 4)
+ethnology = create_lists(df, 'Ethnology', 3)
 
 print(df.head())
 df['vector'] = df.apply(create_row_vectors, axis=1)
 
 print(df['vector'].head(n=50))
-vector_df = df[['publicName', 'translationSetId', 'vector']]
+vector_df = df[['translationSetId', 'vector']]
 
 vector_df.to_csv('vector_csv.csv')
--- temp.py
+++ temp.py
@@ -8,63 +8,163 @@ import shutil
 from datetime import date, datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 currentdate = date.today().strftime('%Y.%m.%d')
 # currentdate = '2019.04.02'
-import json
-SYMBOLS = [' ', '/', '-', '&', ',', '\’','\‘', '\'', "'"]
 
-user_id_list = ['x', 'y']
-
-class Client:
-  	def __init__(self, user_id, country, location):
-		self.id = user_id
-		self.sessions = []
-		self.location = location
-		self.country = country
-	def add_session(self,session):
-		self.sessions.append(session)
-
-	def is_dutch(self):
-		if self.country == 'Netherlands':
-			self.dutch = True
-		else:
-			self.dutch = False
-
+SYMBOLS = [' ', '/', '-', '&', ',', '\’', '\‘', '\'', "'"]
 
 
 def move_files(filename):
-	shutil.move("%s/%s" %(fileDir, filename), "%s/RESULTS/%s" %(fileDir, filename))
+    shutil.move("%s/%s" % (fileDir, filename), "%s/RESULTS/%s" % (fileDir, filename))
+
 
 def create_file(filename, df):
-	df.to_excel(filename, index=False)
-	move_files(filename)
+    df.to_excel(filename, index=False)
+    move_files(filename)
+
 
 def clean_column(df, column):
-	for symbol in SYMBOLS:
-		df["%s" %column] = df["%s" %column].astype(str).str.replace(r'%s' % symbol,'')
-	return df
+    for symbol in SYMBOLS:
+        df["%s" % column] = df["%s" % column].astype(str).str.replace(r'%s' % symbol, '')
+    return df
+
 
 ''' STANDAARD '''
+from ast import literal_eval
 
-def get_city_list():
-	df = pd.read_csv(f"{fileDir}/Data/gemeentes.csv", header=0)
+def create_true_falses_multiplication(row):
+    data = literal_eval(row['facilities'])
 
-	df.drop(['Gemeentecode'], axis=1, inplace=True)
-	return df
+    # These are the vector updates based on the facilities column
+    if 'MuseumLibrary' in data:
+        row['bieb'] = 1
+    else:
+        row['bieb'] = 0
+    if 'OpenAir' in data:
+        row['openair'] = 1
+    else:
+        row['openair'] = 0
+    if 'Parking' in data:
+        row['parking'] = 1
+    else:
+        row['parking'] = 0
+    if 'WheelchairAccess' in data:
+        row['weelchair'] = 1
+    else:
+        row['weelchair'] = 0
+    if 'AccessibleDisabled' in data:
+        row['disabled'] = 1
+    else:
+        row['disabled'] = 0
+    if 'NearTrainStation' in data:
+        row['trainstation'] = 1
+    else:
+        row['trainstation'] = 0
+    if 'Restaurant' in data:
+        row['restaurant'] = 1
+    else:
+        row['restaurant'] = 0
 
-def new_user(client):
-	global user_id_list
+    # These are the vector updates based on the categories found in the external museum data file
 
-	user_id_list.append(client.id)
-	client.is_dutch()
+    return row
 
-def initial_run(client, sessionid, city):
-	global user_id_list, sessions_id_list
+def create_dataframe(filename):
 
-	if client.id not in user_id_list:
-		new_user(client)
+    df = pd.read_csv(filename, encoding='utf-8')
+    df_cleaned = df.drop_duplicates(subset=['publicName'])
+    print(len(df_cleaned))
+    return df_cleaned
+
+def apply_onehot(df):
+    one_hot_cat = df.museaal_thema.str.get_dummies(', ')
+    df = pd.concat([df, one_hot_cat], axis=1)
+    print(df.info())
+    return df
+
+def create_lists(df, x, addition):
+    mylist = df[f'{x}'].tolist()
+    myarray = np.array(mylist)
+    myarray *= addition
+    myarray += 1
+    return myarray
 
 
-initial_run(1,2,3)
+def create_row_vectors(row):
+
+    index = row['index']
+    vector = np.zeros(496, dtype=object)
+    vector += 1
+    if row['bieb'] != 0:
+        vector *= bieb_array
+    if row['openair'] != 0:
+        vector *= openair_array
+    if row['parking'] != 0:
+        vector *= parking_array
+    if row['weelchair'] != 0:
+        vector *= weelchair_array
+    if row['disabled'] != 0:
+        vector *= disabled_array
+    if row['trainstation'] != 0:
+        vector *= trainstation_array
+    if row['restaurant'] != 0:
+        vector *= restaurant_array
+    if row['History'] != 0:
+        vector *= restaurant_array
+    if row['Visual'] != 0:
+        vector *= restaurant_array
+    if row['Culture'] != 0:
+        vector *= restaurant_array
+    if row['Naval'] != 0:
+        vector *= restaurant_array
+    if row['Nature'] != 0:
+        vector *= restaurant_array
+    if row['Tech'] != 0:
+        vector *= restaurant_array
+    if row['Ethnology'] != 0:
+        vector *= restaurant_array
+
+    vector[index] = 1
+    return vector
+
+# Create the dataframe from the museum file and so some cleaning
+filename = 'musea.csv'
+df = create_dataframe(filename)
+df = df.drop_duplicates(subset=['publicName'])
+df.rename(columns={'Unnamed: 0': "index"}, inplace=True)
+# Import the extra categories from the visitor file
+df_visitor = create_dataframe('Notebooks/museua_visitors.csv')
+df = df.merge(df_visitor, how='left', left_on='translationSetId', right_on='translationSetId')
+df = df.fillna(0)
+print(df.shape)
+print(df)
+# Add all the facilites as on its own columns
+df[['bieb', 'openair', 'parking', 'weelchair', 'disabled', 'trainstation', 'restaurant']] = False
+# For every facility add the increase/weight/update value which will be converted to the update arrays
+df = df.apply(create_true_falses_multiplication, axis=1)
+
+# Create the update arrays for all the facilities
+bieb_array = create_lists(df, 'bieb', 2)
+openair_array = create_lists(df, 'openair', 10)
+parking_array = create_lists(df, 'parking', 8)
+weelchair_array = create_lists(df, 'weelchair', 12)
+disabled_array = create_lists(df, 'disabled', 15)
+trainstation_array = create_lists(df, 'trainstation', 7)
+restaurant_array = create_lists(df, 'restaurant', 1)
+history = create_lists(df, 'History', 2)
+visual = create_lists(df, 'Visual', 3)
+culture = create_lists(df, 'Culture', 3)
+naval = create_lists(df, 'Naval', 4)
+nature = create_lists(df, 'Nature', 4)
+tech = create_lists(df, 'Tech', 4)
+ethnology = create_lists(df, 'Ethnology', 3)
+
+print(df.head())
+df['vector'] = df.apply(create_row_vectors, axis=1)
+
+print(df['vector'].head(n=50))
+vector_df = df[['translationSetId', 'vector']]
+
+vector_df.to_csv('vector_csv.csv')
